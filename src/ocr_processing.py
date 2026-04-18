fb255cda62d23684008d0c57dd433bd6b4317846
Sarah Hamdi <155466995+sachoumdh@users.noreply.github.com>
2026-04-18 12:32:44 +0100

    Add Grayscale Image Processing 

--- src/ocr_processing.py
+++ src/ocr_processing.py
@@ -1,20 +1,23 @@
 import cv2 as cv
 import cv2
 import pytesseract
-from google.colab.patches import cv2_imshow
 
-img2 = cv.imread('/content/AI-School-Epoch1-Checkpoint4/data/image2.png')
-cv2_imshow(img2)
+img3 = cv.imread('data/image3.png')
+cv.imshow('Original Image', img3)
+cv.waitKey(0)
+cv.destroyAllWindows()
 
-img2.shape
+print(f"Original Image Dimensions : {img3.shape}")
 
-text1 = pytesseract.image_to_string(img2, lang='eng').replace('\n',' ')
+text1 = pytesseract.image_to_string(img3, lang='eng').replace('\n',' ')
 print(text1)
 
-gray = cv.cvtColor(img2, cv.COLOR_RGB2GRAY)
-cv2_imshow(gray)
+gray = cv.cvtColor(img3, cv.COLOR_RGB2GRAY)
+cv.imshow('Grayscale Image', gray)
+cv.waitKey(0)
+cv.destroyAllWindows()
 
-gray.shape
+print(f"Grayscale Image Dimensions : {gray.shape}")
 
 text2 = pytesseract.image_to_string(gray, lang='eng').replace('\n',' ')
 print(text2)
